0bc87fd25ad9cd19df987af52e5dcbc3900ca98c
Layla Duale <layla.du12@gmail.com>
2021-12-02 04:10:06 +0000

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,15 @@
-function eqArrays(arr1, arr2){
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0, len = arr1.length; i < len; i++){
-      if (arr1[i] !== arr2[i]){
+function eqArrays(arr1,arr2){
+  if(arr1===arr2)
+      return true
+
+  if(arr1.length!=arr2.length)
+      return false
+
+  for(let i=0 ; i<arr1.length ; i++)
+  {
+      if(!eqArrays(arr1[i],arr2[i]))
           return false;
-      }
   }
-  return true; 
+  return true;
 }
-
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  console.log(actual);
-};
+module.exports = eqArrays;
